a2452e3adfa6e16e4e0d01287bbe660e42b37895
Abdullah Al Akash <aabdullahalakash@gmail.com>
2021-09-06 14:46:05 +0600

    Practice Start

---
+++ app.js
@@ -0,0 +1,12 @@
+/*
+        1. Show Output After 3.5 Seconds
+        2. Take a number using Prompt and add with 200 and then alert the result.
+        3. Do you want to see the href ? If ok then clg.
+        4. Purposes of cookies. Write it with 5-7 lines.
+        5. 3 differences between localStorage and sessionStorage.
+        6. How javascript run? Write it with 5-6 lines.
+        7. List of things that are asynchronous in js/browser.
+        8. What is event loop in javascript and how does it work?
+        9. Simple Website with two input fields(Product name & Product price) and single button. By clicking on the button product will be added the localStorage. You should be able to add the multiple.
+        10. Display products on the website UI if they are exists in localStorage.
+*/
